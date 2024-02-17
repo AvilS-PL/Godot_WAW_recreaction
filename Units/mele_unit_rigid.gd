55dc64b05614014a8da8b894688b891c27d5d8f2
mateusz <mateuszeksli@o2.pl>
2024-02-17 16:20:43 +0100

    v0.2.1 added fight mechanics

--- Units/mele_unit_rigid.gd
+++ Units/mele_unit_rigid.gd
@@ -1,16 +1,53 @@
 extends RigidBody2D
 
-@export var speed = 100
+@export var speed = 100.0
+@export var health = 10
+var new_speed = 100.0
 var destinition = Vector2.ZERO
+var team = "blue"
+
+var enemies = []
 
 func _ready():
+	if team == "red":
+		$Body.modulate = Color(0.8,0.2,0.2)
+		$HitBox.collision_layer = 2
+		#await get_tree().create_timer(5.0).timeout
+		#queue_free()
+	elif team == "blue":
+		$Body.modulate = Color(0.0,0.6,0.9)
+		$HitBox.collision_layer = 1
 	pass
 
+
 func _process(delta):
+	if health < 1:
+		queue_free()
+	speed = move_toward(speed, new_speed, 5.0)
 	linear_velocity = (destinition - position).normalized() * speed
-	if (destinition.x > position.x):
+	if destinition.x > position.x:
 		$Body.flip_h = false
 		$Head.flip_h = false
 	else:
 		$Body.flip_h = true
 		$Head.flip_h = true
+
+
+func _on_hit_box_area_entered(area):
+	if $HitBox.collision_layer != area.collision_layer:
+		if enemies.size() == 0:
+			$Fight.start()
+		new_speed = 0.0
+		enemies.append(area)
+
+func _on_hit_box_area_exited(area):
+	if $HitBox.collision_layer != area.collision_layer:
+		enemies.remove_at(enemies.find(area, 0))
+		if enemies.size() == 0:
+			new_speed = 100.0
+
+
+func _on_fight_timeout():
+	if enemies.size() != 0:
+		enemies[0].get_parent().health -= 1
+		$Fight.start()
--- game.gd
+++ game.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
 var mele_unit = preload("res://Units/mele_unit_rigid.tscn")
+var mode = true
 
 func _process(delta):
-	if Input.is_action_just_pressed("mouse_left_click"):
-		createUnit(mele_unit, "blue", get_global_mouse_position(), $MarkerEnemy.position)
-
-	if Input.is_action_just_pressed("mouse_right_click"):
-		createUnit(mele_unit, "red", get_global_mouse_position(), $MarkerBase.position)
-
+	if mode: 
+		if Input.is_action_just_pressed("mouse_left_click"):
+			createUnit(mele_unit, "blue", get_global_mouse_position(), $MarkerEnemy.position)
+		if Input.is_action_just_pressed("mouse_right_click"):
+			createUnit(mele_unit, "red", get_global_mouse_position(), $MarkerBase.position)
+	else:
+		if Input.is_mouse_button_pressed(1):
+			createUnit(mele_unit, "blue", get_global_mouse_position(), $MarkerEnemy.position)
+		if Input.is_mouse_button_pressed(2):
+			createUnit(mele_unit, "red", get_global_mouse_position(), $MarkerBase.position)
+			
+	if Input.is_action_just_pressed("ui_accept"):
+		mode = !mode
 
 func createUnit(type, team, posi, dest):
 	var unit = type.instantiate()
 	unit.position = posi
 	unit.destinition = dest
+	unit.team = team
 	add_child(unit)
