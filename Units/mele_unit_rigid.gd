b02e93afe728062219899a2a188515b7eba47e4b
mateusz <mateuszeksli@o2.pl>
2024-02-17 22:14:48 +0100

    v0.2.5 hand animations but cooldown problems

--- Units/mele_unit_rigid.gd
+++ Units/mele_unit_rigid.gd
@@ -1,17 +1,19 @@
 extends RigidBody2D
 
-@export var speed = 100.0
+#popracuj nad cooldown i wiążącymi się z tym zależnościami
+
+var speed = 100.0
 var new_speed = speed
-@export var max_health = 10
+var max_health = 10.0
 var health = max_health
-@export var damage = 3
-@export var cooldown = 1
+var damage = 5.0
+var cooldown = 1.0
 var destinition = Vector2.ZERO
 var team = "blue"
-
 var enemies = []
 
 func _ready():
+	$AnimationPlayer.speed_scale = 1.1 / cooldown
 	$HealthBar.max_value = health
 	$HealthBar.value = health
 	$Fight.wait_time = cooldown
@@ -27,12 +29,17 @@ func _ready():
 func _process(delta):
 	speed = move_toward(speed, new_speed, 5.0)
 	linear_velocity = (destinition - position).normalized() * speed
-	if destinition.x > position.x:
+	#if destinition.x > position.x:
+	if team == "blue":
 		$Body.flip_h = false
 		$Head.flip_h = false
+		$Hand.flip_h = false
+		$Hand.position.x = 40
 	else:
 		$Body.flip_h = true
 		$Head.flip_h = true
+		$Hand.flip_h = true
+		$Hand.position.x = -40
 
 func _on_hit_box_area_entered(area):
 	if $HitBox.collision_layer != area.collision_layer:
@@ -49,19 +56,18 @@ func _on_hit_box_area_exited(area):
 
 func _on_fight_timeout():
 	if enemies.size() != 0:
-		enemies[0].get_parent().take_damage(damage)
+		enemies[0].get_parent().take_damage(damage, cooldown/2)
+		if team == "blue":
+			$AnimationPlayer.play("punch")
+		else:
+			$AnimationPlayer.play("punch_left")
 		$Fight.start()
 
-func take_damage(taken):
+func take_damage(taken, time):
 	health -= taken
 	if health <= 0:
-		var time
-		if health == 0:
-			time = 0.4
-		else:
-			time = 0.4 * (1 - float(abs(health))/float(taken))
-		$HealthBar.change_health(health, time)
-		await get_tree().create_timer(time).timeout
+		$HealthBar.change_health(health, time/2)
+		await get_tree().create_timer(time/2).timeout
 		queue_free()
 	else:
-		$HealthBar.change_health(health, 0.4)
+		$HealthBar.change_health(health, time)
--- Usables/health_bar.gd
+++ Usables/health_bar.gd
@@ -11,4 +11,4 @@ func change_health(health, time):
 	sb.bg_color = Color(1 - ((health/max_value) * (health/max_value) * (health/ max_value)),0.1 + ((health/ max_value)),0.0)
 	if health < 0:
 		health = 0
-	tween.tween_property(self, "value", health, time).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "value", health, time).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
