14daa20d6a6150bcb4aff203f5a142a11ec0b720
mateusz <mateuszeksli@o2.pl>
2024-02-18 18:45:45 +0100

    v0.3.2 redesigned mele unit and added death particle effect

--- Units/mele_units/mele_unit_rigid.gd
+++ Units/mele_units/mele_unit_rigid.gd
@@ -1,15 +1,19 @@
 extends RigidBody2D
 
-#popracuj nad cooldown i wiążącymi się z tym zależnościami
+#await get_tree().create_timer(5.0).timeout
 
-var speed = 100.0
-var new_speed = speed
-var max_health = 10.0
-var health = max_health
-var damage = 3.0
-var cooldown = 1.0
-var destinition = Vector2.ZERO
 var team = "blue"
+var destinition = Vector2.ZERO
+
+var animation_name = "punch"
+var speed = 100.0
+var max_health = 10.0
+var damage = 5.0
+var cooldown = 0.5
+
+var new_speed = speed
+var health = max_health
+
 var enemies = []
 
 func _ready():
@@ -18,30 +22,17 @@ func _ready():
 	$HealthBar.value = health
 	$Fight.wait_time = cooldown
 	if team == "red":
-		$Body.modulate = Color(0.8,0.2,0.2)
+		$Side/Body.modulate = Color(0.8,0.2,0.2)
 		$HitBox.collision_layer = 2
-		#$HitBox.collision_mask = 1
-		#await get_tree().create_timer(5.0).timeout
-		#queue_free()
+		$Side.scale.x = -1
 	elif team == "blue":
-		$Body.modulate = Color(0.0,0.6,0.9)
+		$Side/Body.modulate = Color(0.0,0.6,0.9)
 		$HitBox.collision_layer = 1
-		#$HitBox.collision_mask = 2
+		$Side.scale.x = 1
 
 func _process(delta):
 	speed = move_toward(speed, new_speed, 5.0)
 	linear_velocity = (destinition - position).normalized() * speed
-	#if destinition.x > position.x:
-	if team == "blue":
-		$Body.flip_h = false
-		$Head.flip_h = false
-		$Hand.flip_h = false
-		$Hand.position.x = 40
-	else:
-		$Body.flip_h = true
-		$Head.flip_h = true
-		$Hand.flip_h = true
-		$Hand.position.x = -40
 
 func _on_hit_box_area_entered(area):
 	if $HitBox.collision_layer != area.collision_layer:
@@ -58,18 +49,25 @@ func _on_hit_box_area_exited(area):
 
 func _on_fight_timeout():
 	if enemies.size() != 0:
-		enemies[0].get_parent().take_damage(damage, cooldown/2)
-		if team == "blue":
-			$AnimationPlayer.play("punch")
-		else:
-			$AnimationPlayer.play("punch_left")
+		$AnimationPlayer.play("punch")
+
+func deal_damage():
+	if enemies.size() != 0:
+		enemies[0].get_parent().take_damage(damage, cooldown)
 		$Fight.start()
 
 func take_damage(taken, time):
 	health -= taken
+	
 	if health <= 0:
-		$HealthBar.change_health(health, time/2)
-		await get_tree().create_timer(time/4).timeout
+		$HealthBar.change_health(health, time/4)
+		#await get_tree().create_timer(cooldown / 12).timeout #to jeszcze do zdecydowania czy zostawić
+		var deadEffect = load("res://Usables/blood_splash.tscn")
+		var deadEffectI = deadEffect.instantiate()
+		deadEffectI.global_position = global_position
+		var world = get_tree().current_scene
+		world.add_child(deadEffectI)
+		
 		queue_free()
 	else:
-		$HealthBar.change_health(health, time)
+		$HealthBar.change_health(health, time/2)
