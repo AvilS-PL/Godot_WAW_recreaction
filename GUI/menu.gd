5299f5f76ca325337a466ef8101327076f6f962d
mateusz <mateuszeksli@o2.pl>
2024-02-20 23:26:03 +0100

    v0.5.3 added a few things in menu

--- GUI/menu.gd
+++ GUI/menu.gd
@@ -2,6 +2,11 @@ extends Control
 
 var place = "startMenu"
 
+#func _ready():
+	#$PlayAnimation.speed_scale = 2
+	#$PlayAnimation.play("play")
+	#place = "mainMenu"
+
 func _on_play_pressed():
 	if place == "startMenu":
 		place = "mainMenu"
@@ -14,8 +19,10 @@ func _on_text_pressed():
 func reset_text_xd():
 	$StartMenu/Text.rotation = 0
 
-
 func _on_back_pressed():
 	if place == "mainMenu":
 		place = "startMenu"
 		$PlayAnimation.play("replay")
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
