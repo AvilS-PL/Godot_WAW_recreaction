6c36c9aa6f0bf1c2621f4124149fc1be56b1bb44
mateusz <mateuszeksli@o2.pl>
2024-02-21 02:40:44 +0100

    v0.5.4 added new textures

--- GUI/menu.gd
+++ GUI/menu.gd
@@ -1,28 +1,3 @@
 extends Control
 
 var place = "startMenu"
-
-#func _ready():
-	#$PlayAnimation.speed_scale = 2
-	#$PlayAnimation.play("play")
-	#place = "mainMenu"
-
-func _on_play_pressed():
-	if place == "startMenu":
-		place = "mainMenu"
-		$PlayAnimation.play("play")
-
-func _on_text_pressed():
-	#tutaj wraz z tą animacją może się np tło zmieniać
-	$StartMenu/TextAnimation.play("rotate")
-
-func reset_text_xd():
-	$StartMenu/Text.rotation = 0
-
-func _on_back_pressed():
-	if place == "mainMenu":
-		place = "startMenu"
-		$PlayAnimation.play("replay")
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
